e9c9d984db61f29c9e189beba98190b2124dbe98
Miguel Ángel Fernández Carrillo <miguelangel.fernandez.practicas@telefonica.com>
2021-03-04 00:31:55 +0100

    Ex01 finished

---
+++ ex01/ex01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+void	memoryLeak()
+{
+	std::string *panthere = new std::string("String panthere");
+	std::cout << *panthere << std::endl;
+	delete (panthere);
+}
+
+int main(void)
+{
+	memoryLeak();
+	return (0);
+}
